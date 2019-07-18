030bae9d2e5b9c1db85ca27d615fe67f13fc53ea
Derek Miller <derekmlmt@yahoo.com>
2019-07-18 12:55:02 -0700

    Expanded logic, console logs added at each step of conditionals to determine path of flow.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,16 @@ var wins =0;
 var losses = 0;
 var guesses = 10;
 var random = "";
+var answer = "";
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-var guessed = [" "]
+var wrong = [" "];
 
 document.querySelector('.wins').innerText = wins;
 document.querySelector('.losses').innerText = losses;
 document.querySelector('.guesses').innerText =  guesses;
-document.querySelector('.guessed-letters').innerHTML = guessed;
-
+document.querySelector('.guessed-letters').innerHTML = wrong;
+var answer = document.querySelector('.answer');
 
 // function to choose a letter from alphabet, store it to variable
  function psychicGame() {
@@ -24,24 +25,49 @@ document.querySelector('.guessed-letters').innerHTML = guessed;
 
 // event listener for user guess including conditionals
 
-        var guess= document.onkeyup = function(e) {
-            e.key.toLowerCase();
+        document.onkeyup = function(e) {
+            var guess = e.key.toLowerCase();
+        console.log("you guessed " + guess);
+            // conditional for verifying that is a letter and determining if it was guessed already
+
+            if (letters.includes(guess)) {
+                console.log("guess is a valid letter");
+
+            // conditional for answer check comparison
+                        if (guess === random) {
+                            console.log("Correct guess");
+                            wins++;
+                            answer.innerText = guess;
+                        }   
+    
+                    if (guess !== random) {
+                        
+                        if (wrong.indexOf(guess) === -1) {
+                            wrong.push(guess);
+                            console.log("wrong guess, subtracting 1")
+                            guesses--;
+                            console.log("guesses--;")
         
-// conditional for guessed array, checking to see if it is already inside
+                            if(wrong.length === 9) {
+                                losses++;
+                                guesses=10;
+                                wrong = [""];
+                                console.log("losses++;")
+                                psychicGame();
+                            }
+                        }
+                    }
+                }
 
-            if (guessed.indexOf(guess) === -1) {
-                guessed.push(guess)
             }
 
-// conditional for answer check 
 
-            if (letters.includes(guess)){
-                if (guess !== random) {
-                guesses--;
-                guessed.push(guess);
-            }
-        }
-    }
+
+                
+            
+         
+
+    
 
 
 
